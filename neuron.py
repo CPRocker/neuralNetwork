a7daa637403e88ad29e99b75d13265c147f23739
Carson Price <carsonp@sio.midco.net>
2020-12-29 14:15:07 -0600

    Finished part 6 with softmax activation of outputs.

--- neuron.py
+++ neuron.py
@@ -1,8 +1,6 @@
 import numpy as np
 from cd import createData
 
-X, y = createData(100, 3)
-
 class LayerDense:
     def __init__(self, nInputs, nNeurons):
         self.weights = 0.1 * np.random.randn(nInputs, nNeurons)
@@ -14,9 +12,24 @@ class ActivationReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = LayerDense(2, 5)
+class ActivationSoftmax:
+    def forward(self, inputs):
+        expValues = np.exp(inputs - np.max(inputs, axis = 1, keepdims = True))
+        probabilities = expValues / np.sum(expValues, axis = 1, keepdims = True)
+        self.output = probabilities
+
+X, y = createData(100, 3)
+
+dense1 = LayerDense(2, 3)
 activation1 = ActivationReLU()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = LayerDense(3, 3)
+activation2 = ActivationSoftmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
